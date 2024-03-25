12fbcdc0c6d2ca550c34d5ae14a115674a23e698
Yannis Lebbar <yannislebbar@gmail.com>
2024-03-25 16:52:41 -0400

    Created a new class called LiftPoint. Creates a point object that will be used in the chairlift class. Captures relevant user info at a point in time when they are on a chairlift.

---
+++ src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/LiftPoint.java
@@ -0,0 +1,35 @@
+package de.storchp.opentracks.osmplugin.dashboardapi;
+import java.sql.Time;
+import java.time.LocalTime;
+
+public class LiftPoint {
+    private double xCoordinate;
+    private double yCoordinate;
+    private Time currentTime;
+
+    public LiftPoint() {
+
+    }
+
+    public LiftPoint(double xCoordinate, double yCoordinate, double currentSpeed, Time currentTime) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.currentTime = currentTime;
+    }
+
+    public double getXCoordinate() {
+        return this.xCoordinate;
+    }
+
+    public double getYCoordinate() {
+        return this.yCoordinate;
+    }
+
+    public double getCurrentSpeed() {
+        return this.currentSpeed;
+    }
+
+    public Time getCurrentTime() {
+        return this.currentTime;
+    }
+}
