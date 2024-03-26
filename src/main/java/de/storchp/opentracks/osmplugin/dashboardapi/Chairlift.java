3727e54277e432a4b7a2c26a7ea5d933a8f61291
Negar Torab Parhiz <74454052+ragenpt@users.noreply.github.com>
2024-03-25 20:42:22 -0400

    Update Chairlift.java
    Adding a temporary text based table.

--- src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/Chairlift.java
+++ src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/Chairlift.java
@@ -56,4 +56,17 @@ public class Chairlift {
     public void setAverageSpeed(double averageSpeed) {
         this.averageSpeed = averageSpeed;
     }
+    // Method to display chairlift information in a table
+    // To be replaced with a GUI based table
+    public void displayTable() {
+        System.out.println("+---------------------------------------+");
+        System.out.println("|            Chairlift Info             |");
+        System.out.println("+---------------------------------------+");
+        System.out.printf("| %-20s | %-10s |\n", "Name", name);
+        System.out.printf("| %-20s | %-10.2f |\n", "Elevation Gain", elevationGain);
+        System.out.printf("| %-20s | %-10d |\n", "Waiting Time", waitingTime);
+        System.out.printf("| %-20s | %-10d |\n", "Total Time Moving", totalTimeMoving);
+        System.out.printf("| %-20s | %-10.2f |\n", "Average Speed", averageSpeed);
+        System.out.println("+---------------------------------------+");
+    }
 }
