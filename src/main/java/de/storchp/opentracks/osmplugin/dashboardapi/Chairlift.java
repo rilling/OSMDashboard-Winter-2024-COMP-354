4a0fe9b2574275b59da553f96b1737ca275d0271
nandrN <158762206+nandrn@users.noreply.github.com>
2024-04-08 22:54:14 -0400

    Adapted the code from SegmentAdapter implemented by another team, and RunAdapter adapted from other teammembers, for chairliftAdapter, to implement the same functionalities from these 2 classes for the chairlifts. Code is adapted for the needs of chairlifts, to bind the data to the table displaying the information, and to color a specific chairlift path when user selects it, according to its start and end points. Chairlift class, added 2 methods to get the start and end points of the chairlift, adapted the code from Run class, to implement the same functionality but for chairlifts, instead of runs.

--- src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/Chairlift.java
+++ src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/Chairlift.java
@@ -1,5 +1,8 @@
 package de.storchp.opentracks.osmplugin.dashboardapi;
 
+import org.oscim.core.GeoPoint;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Chairlift {
@@ -9,12 +12,18 @@ public class Chairlift {
     private double totalTimeMoving;
     private double averageSpeed;
 
+    private ArrayList<TrackPoint> trackPointCollection;
+    private double[] lat;
+    private double[] lon;
+
     public Chairlift(String name, double eGain, double wTime, double tTimeMoving, double aSpeed) {
         this.name = name;
         this.elevationGain = eGain;
         this.waitingTime = wTime;
         this.totalTimeMoving = tTimeMoving;
         this.averageSpeed = aSpeed;
+        this.lat = new double[trackPointCollection.size()];
+        this.lon = new double[trackPointCollection.size()];
     }
 
     public Chairlift(String name, double eGain, double wTime, double tTimeMoving) {
@@ -22,6 +31,7 @@ public class Chairlift {
         this.elevationGain = eGain;
         this.waitingTime = wTime;
         this.totalTimeMoving = tTimeMoving;
+        this.trackPointCollection = null;
     }
 
     public String getName() {
@@ -63,4 +73,25 @@ public class Chairlift {
     public void setAverageSpeed(double averageSpeed) {
         this.averageSpeed = averageSpeed;
     }
+
+    // Method to get the end point of the run
+
+    //code adapted/taken from Run class to get start point to implement this functionality for chairlift.
+    public GeoPoint getStartPoint() {
+        if (trackPointCollection != null && !trackPointCollection.isEmpty()) {
+            return trackPointCollection.get(0).getLatLong();
+        }
+        else {
+            return null; //or handle this case as per your requirement
+        }
+    };
+    //code adapted/taken from Run class to get the end point to implement this functionality for chairlift.
+    public GeoPoint getEndPoint() {
+        if (trackPointCollection != null && !trackPointCollection.isEmpty()) {
+            return trackPointCollection.get(trackPointCollection.size()-1).getLatLong();
+        }
+        else {
+            return null; //or handle this case as per your requirement
+        }
+    };
 }
--- src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/ChairliftAdapter.java
+++ src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/ChairliftAdapter.java
@@ -28,6 +28,7 @@ import org.oscim.utils.animation.Easing;
 
 import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Locale;
 
 import de.storchp.opentracks.osmplugin.R;
 import de.storchp.opentracks.osmplugin.maps.MovementDirection;
@@ -90,7 +91,7 @@ public class ChairliftAdapter extends RecyclerView.Adapter<ChairliftAdapter.Chai
 
         private TextView avgSpeed;
 
-        private TextView distance;
+        private TextView elevationGain;
 
         private TableLayout chairliftView;
 
@@ -100,8 +101,80 @@ public class ChairliftAdapter extends RecyclerView.Adapter<ChairliftAdapter.Chai
             name = itemView.findViewById(R.id.item_Name);
             waitingTime = itemView.findViewById(R.id.wTime);
             avgSpeed = itemView.findViewById(R.id.avgSpeedInput);
-            distance = itemView.findViewById(R.id.distance);
+            elevationGain = itemView.findViewById(R.id.distanceInput);
             chairliftView = itemView.findViewById(R.id.chairLiftView);
         }
+
+        //bind method adapted from SegmentAdapter and RunAdapter classes to implement same functionalities but for chairlift class.
+        //code adapted from SegmentAdapter and RunAdapter and adapted for chairlifts.
+        public void bind(Chairlift chairlift) {
+            name.setText(chairlift.getName());
+            waitingTime.setText(formatDuration(chairlift.getWaitingTime()));
+            avgSpeed.setText(String.format(Locale.getDefault(),"%.2f km/h", chairlift.getAverageSpeed()));
+            elevationGain.setText(String.format(Locale.getDefault(), "%.2f meters", chairlift.getElevationGain()));
+
+            chairliftView.setOnClickListener( v -> {
+                Toast.makeText(v.getContext(), chairlift.getName() + " Clicked", Toast.LENGTH_SHORT).show();
+                chairliftView.setBackgroundColor(Color.rgb(212, 221, 232));
+
+                //Removing previous layer if any
+                mapView.map().layers().remove(polylinesLayer);
+
+                for (int i = 0; i < polylinesLayer.map().layers().size(); i++) {
+                    Layer layer = polylinesLayer.map().layers().get(i);
+                    if (layer instanceof PathLayer) {
+                        polylinesLayer.map().layers().remove(i);
+                    }
+                }
+
+                PathLayer line = new PathLayer(mapView.map(), Color.BLUE, 12);
+                //Add points to the PathLayer
+                line.addPoint(chairlift.getStartPoint());
+                line.addPoint(chairlift.getEndPoint());
+
+                polylinesLayer.map().layers().add(line);
+                mapView.map().layers().add(polylinesLayer);
+
+                List<GeoPoint> latLongs = line.getPoints();
+                Waypoint startWayPoint = new Waypoint(chairlift.getStartPoint(), "start point");
+                Waypoint endWayPoint = new Waypoint(chairlift.getEndPoint(), "end point");
+
+                final MarkerItem startPin = MapUtils.createTappableMarker(mapView.getContext(), startWayPoint);
+                final MarkerItem endPin = MapUtils.createTappableMarker(mapView.getContext(), endWayPoint);
+                MarkerSymbol greenSymbol = MapUtils.createMarkerSymbol(mapView.getContext(), R.drawable.ic_marker_green_pushpin_modern, false, MarkerSymbol.HotspotPlace.CENTER);
+                MarkerSymbol redSymbol = MapUtils.createMarkerSymbol(mapView.getContext(), R.drawable.ic_marker_red_pushpin_modern, false, MarkerSymbol.HotspotPlace.CENTER);
+                startPin.setMarker(greenSymbol);
+                endPin.setMarker(redSymbol);
+
+                if (waypointsLayer != null) {
+                    mapView.map().layers().remove(waypointsLayer);
+                }
+
+                waypointsLayer = createWaypointsLayer();
+                mapView.map().layers().add(waypointsLayer);
+                waypointsLayer.addItem(startPin);
+                waypointsLayer.addItem(endPin);
+
+                BoundingBox boundingBox = new BoundingBox(latLongs);
+                updateMapPositionAndRotation(boundingBox.getCenterPoint());
+                mapView.map().animator().animateTo(500, boundingBox, Easing.Type.LINEAR, ANIM_MOVE);
+
+            });
+
+        }
+    }
+
+    //code adapted/taken from RunAdapter and SegmentAdapter classes
+    private ItemizedLayer createWaypointsLayer() {
+        MarkerSymbol symbol = MapUtils.createPushpinSymbol(mapView.getContext());
+        return new ItemizedLayer(mapView.map(), symbol);
+    }
+    //code adapted/taken from RunAdapter and SegmentAdapter classes
+    private void updateMapPositionAndRotation(final GeoPoint myPos){
+        mapView.map().getMapPosition().setPosition(myPos).setBearing(mapMode.getHeading(movementDirection));
+    }
+    //code adapted/taken from RunAdapter class
+    private String formatDuration(long durationInSeconds) {
+        return DateUtils.formatElapsedTime(durationInSeconds);
     }
 }
